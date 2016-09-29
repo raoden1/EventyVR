f64b957b68976f347f46b1502698e4f5fb52548e
[RaOMIME] <michal.berner@outlook.com>
2016-09-29 03:47:17 +0200

    basic files another in progress

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ $(document).ready(function(e) {
+    e.preventDefault;
+    $('nav').show(1000).slideDown(6000);
+    $('#introduction').show(2000).slideDown(4000);
+
+  
+
+ 
+    
+    
+    
+  });
